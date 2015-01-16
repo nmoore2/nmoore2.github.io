edbb4affe2ef033b3b062e8d4d67817c2283c918
Nathan Moore <n@nmoore.net>
2015-01-16 14:48:51 -0700

    update google analytics

--- index.php
+++ index.php
@@ -57,8 +57,10 @@
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		  ga('create', 'UA-58461736-1', 'auto');
+		  ga('create', 'UA-58635435-1', 'auto');
 		  ga('send', 'pageview');
+
+
 
 		// End Google Analytics
 
